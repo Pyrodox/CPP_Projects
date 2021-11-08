1aead9b43dd7aad42947a49ec8be7ea140c7d9ce
Pyro-Impact <chaosdragonn1@gmail.com>
2021-11-07 23:39:25 -0800

    ez

---
+++ CurrencyConverter.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using std::cout; using std::cin;
+
+int main()
+{
+    
+
+    return 0;
+}
---
+++ GUICalc.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string input;
+    cin >> input;
+    for (int i = 0; i < input.length(); ++i) {
+        char const &c = input.at(i);
+        if (isdigit(c)) {
+            
+        }
+    }
+}
