cc9d05452fe8b7b612439443b9ce43e211b4600b
Pyro-Impact <chaosdragonn1@gmail.com>
2021-11-08 00:10:01 -0800

    ez

--- GUICalc.cpp
+++ GUICalc.cpp
@@ -3,12 +3,22 @@ using namespace std;
 
 int main()
 {
+    vector<char> v {'+', '-', '*', '/', '^', '!'};
+    //make a map of char and int for operator precedence
     string input;
+    stack<char> operate;
+    queue<int> que;
     cin >> input;
     for (int i = 0; i < input.length(); ++i) {
         char const &c = input.at(i);
         if (isdigit(c)) {
-            
+            que.push(int(c));
+        }
+        //add function option later, not ready yet
+        else if (find(v.begin(), v.end(), c) != v.end()) {
+            while (operate.top() != '(' && (operate.top() > c || c == operate.top())) {
+
+            }
         }
     }
 }
